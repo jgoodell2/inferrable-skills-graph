339afec0781028037fd64c2713dcf5e67360b832
jgoodell <jgoodell2@yahoo.com>
2025-01-22 17:07:33 -0500

    Delivery of Skills Graph Demo unsing INFERable detector.

---
+++ js/jimsxapiwrapper.js
@@ -0,0 +1,49 @@
+
+function setupSimpleXAPI(actorName, actorOpenId, verbId, verbDisplay, objectId, objectName, objectDescription, resultResponseValue, resultSuccessValue, contextActivity, agent){
+
+         let userAgent = JSON.stringify(navigator.userAgent);
+         
+         var statement = {
+                  "actor": {
+                           "openid": actorOpenId,
+                           "name": actorName,
+                           "objectType": "Agent"
+                  },
+                  "verb": {
+                           "id": verbId,
+                           "display": {"en-US": verbDisplay}
+                  },
+                  "object": {
+                           "id": objectId,
+                           "objectType": "Activity",
+                           "definition": {
+                             "name": {"en-US": objectName},
+                             "description": {"en-US": objectDescription}
+                           }
+                  },
+                  "context": {
+                           "contextActivities": {
+                                    "parent": [
+                                    {
+                                    "id": contextActivity,
+                                    "objectType": "ActivityTemplate" 
+                                    }
+                                    ]
+                           },
+                           "extensions": {
+                                    "https://credreg.net/ctdlasn/terms/Competency#alignTo": "https://credentialengineregistry.org/resources/ce-4451c5bf-6e1f-4935-9e63-e1c4f18b6e18",
+                                    "https://w3id.org/xapi/netc/extensions/user-agent": navigator.userAgent
+                           }
+                  },
+                  "result": {
+                           "response": resultResponseValue,
+                           "success": resultSuccessValue
+                  },
+         }; //end statement definition
+                  
+         console.log(JSON.stringify(statement));
+         return statement;
+
+}
+
+
